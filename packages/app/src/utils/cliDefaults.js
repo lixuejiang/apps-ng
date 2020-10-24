6b44ac7ee30f19a4fada18cfd9ea8fdc0a33ac70
lixuejiang <642898959@qq.com>
2020-10-24 16:22:32 +0800

    merge master

--- packages/app/src/utils/cliDefaults.js
+++ packages/app/src/utils/cliDefaults.js
@@ -1,10 +1,2 @@
-export const CLI_SUBSTRATE_API = (
-  process.env.SUBSTRATE_API 
-  || (process.env.APP_PHALA_URL && `wss://${process.env.APP_PHALA_URL}/ws`)
-  || 'ws://localhost:9944'
-)
-export const CLI_PRUNTIME_API = (
-  process.env.PRUNTIME_API
-  || (process.env.APP_PHALA_URL && `https://${process.env.APP_PHALA_URL}/tee-api/`)
-  || 'http://localhost:8000'
-)
+export const CLI_SUBSTRATE_API = 'ws://68.183.138.126:9944';
+export const CLI_PRUNTIME_API = "http://68.183.138.126:8000";
